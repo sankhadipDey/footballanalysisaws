4aa73042b6ed4bd4280dccbf639c6bd3b15d6da3
sankhadipDey <86321435+sankhadipDey@users.noreply.github.com>
2025-05-31 02:05:51 +0530

    Delete EMR_Transformation.py
    deleting extra files

--- EMR_Transformation.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
-
-spark = SparkSession.builder.appName("FootballETL").getOrCreate()
-df = spark.read.csv("s3://football-project/translate/", header=True, inferSchema=True)
-df = df.withColumn("GoalDiff", col("FTHG") - col("FTAG"))
-df.write.mode("overwrite").parquet("s3://football-project/curated/match_parquet/")
